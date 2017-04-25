9987d1314291a2e8a9d40e9a8260180d1733e655
ypicard <yann.picard95@gmail.com>
2017-04-25 20:25:37 +0200

    Confif env variables for heroku mongo deployemnt

--- app_factory.py
+++ app_factory.py
@@ -1,10 +1,24 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+import os
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 mongo = PyMongo()
 
 
 def create_app():
 	app = Flask(__name__)
+	# Local settings
+	# MONGO_URL = os.environ.get("MONGODB_URI_LOCAL")
+	
+	# Prod
+	MONGO_URL = os.environ.get("MONGODB_URI")
+	
+	logger.info('Connecting to mongo db : ' + MONGO_URL)
+	app.config['MONGO_URI'] =MONGO_URL
+	
 	mongo.init_app(app)
 	return app
--- mongo_set_up.py
+++ mongo_set_up.py
@@ -1,1 +1,2 @@
+# mongod --port 27017 --dbpath=./data
 db.credentials.createIndex({'team_id':1},{unique: true})
