9a32fdf26172f591a890d84ceba0c7b3dc6442d0
ypicard <yann.picard95@gmail.com>
2017-04-28 20:36:35 +0200

    Conf app for dev/prod environments

--- app_factory.py
+++ app_factory.py
@@ -11,14 +11,15 @@ mongo = PyMongo()
 
 def create_app():
 	app = Flask(__name__)
-	# Local settings
-	# MONGO_URL = os.environ.get("MONGODB_URI_LOCAL")
-	
-	# Prod
-	MONGO_URL = os.environ.get("MONGODB_URI")
-	
-	logger.info('Connecting to mongo db : ' + MONGO_URL)
-	app.config['MONGO_URI'] =MONGO_URL
+
+	# Log settings here :
+	logger.info("Current env variables :")
+	logger.info("  - CLIENT_ID : " + os.environ.get("CLIENT_ID"))
+	logger.info("  - CLIENT_SECRET : " + os.environ.get("CLIENT_SECRET"))
+	logger.info("  - VERIFICATION_TOKEN : " + os.environ.get("VERIFICATION_TOKEN"))
+	logger.info("  - MONGODB_URI : " + os.environ.get("MONGODB_URI"))
+
+	app.config['MONGO_URI'] = os.environ.get("MONGODB_URI")
 	
 	mongo.init_app(app)
 	return app
